5b529d941df7188e071868c59a25dc28e3e374c5
techankit09 <ankitrawat0988@gmail.com>
2025-11-03 18:07:49 +0530

    done

---
+++ ORSProject-04/src/main/java/in/co/rays/proj04/bean/CourseBean.java
@@ -0,0 +1,30 @@
+package in.co.rays.proj04.bean;
+
+public class CourseBean {
+
+	private String name;
+	private String duration;
+	private String description;
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDuration() {
+		return duration;
+	}
+	public void setDuration(String duration) {
+		this.duration = duration;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	
+}
---
+++ ORSProject-04/src/main/java/in/co/rays/proj04/bean/RoleBean.java
@@ -0,0 +1,20 @@
+package in.co.rays.proj04.bean;
+
+public class RoleBean {
+	
+	private String name;
+	private String description;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+}
