79193cbb02f638eb85ffffba0c63a3114e8d5784
techankit09 <ankitrawat0988@gmail.com>
2025-12-21 12:42:39 +0530

    done

---
+++ ORSProject-04/src/main/java/in/co/rays/proj4/controller/StaffListCtl.java
@@ -0,0 +1,170 @@
+package in.co.rays.proj4.controller;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import in.co.rays.proj4.bean.BaseBean;
+import in.co.rays.proj4.bean.StaffBean;
+import in.co.rays.proj4.bean.UserBean;
+import in.co.rays.proj4.exception.ApplicationException;
+import in.co.rays.proj4.model.StaffModel;
+import in.co.rays.proj4.model.UserModel;
+import in.co.rays.proj4.util.DataUtility;
+import in.co.rays.proj4.util.PropertyReader;
+import in.co.rays.proj4.util.ServletUtility;
+
+@WebServlet(name = "/StaffListCtl", urlPatterns = { "/ctl/StaffListCtl" })
+public class StaffListCtl extends BaseCtl {
+
+	@Override
+	protected void preload(HttpServletRequest request) {
+
+		HashMap<String, String> map = new HashMap<String, String>();
+		map.put("IT", "IT");
+		map.put("HR", "HR");
+		map.put("FINANCE", "FINANCE");
+		map.put("MARKETING", "MARKETING");
+
+		request.setAttribute("map", map);
+
+	}
+
+	@Override
+	protected BaseBean populateBean(HttpServletRequest request) {
+
+		StaffBean bean = new StaffBean();
+
+		bean.setId(DataUtility.getLong(request.getParameter("id")));
+		bean.setFullName(DataUtility.getString(request.getParameter("fullName")));
+		bean.setJoiningDate(DataUtility.getDate(request.getParameter("joiningDate")));
+		bean.setDivision(DataUtility.getString(request.getParameter("division")));
+		bean.setPreviousEmployer(DataUtility.getString(request.getParameter("Previous Employer")));
+
+		return bean;
+	}
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		int pageNo = 1;
+		int pageSize = DataUtility.getInt(PropertyReader.getValue("page.size"));
+
+		StaffBean bean = (StaffBean) populateBean(request);
+		StaffModel model = new StaffModel();
+
+		try {
+			List<StaffBean> list = model.search(bean, pageNo, pageSize);
+			List<StaffBean> next = model.search(bean, pageNo + 1, pageSize);
+
+			if (list == null || list.isEmpty()) {
+				ServletUtility.setErrorMessage("No record found", request);
+			}
+
+			ServletUtility.setList(list, request);
+			ServletUtility.setPageNo(pageNo, request);
+			ServletUtility.setPageSize(pageSize, request);
+			ServletUtility.setBean(bean, request);
+			request.setAttribute("nextListSize", next.size());
+
+			System.out.println("nextList --->" + next.size());
+
+			ServletUtility.forward(getView(), request, response);
+
+		} catch (ApplicationException e) {
+			e.printStackTrace();
+			ServletUtility.handleException(e, request, response);
+			return;
+		}
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		List list = null;
+		List next = null;
+
+		int pageNo = DataUtility.getInt(request.getParameter("pageNo"));
+		int pageSize = DataUtility.getInt(request.getParameter("pageSize"));
+
+		pageNo = (pageNo == 0) ? 1 : pageNo;
+		pageSize = (pageSize == 0) ? DataUtility.getInt(PropertyReader.getValue("page.size")) : pageSize;
+
+		StaffBean bean = (StaffBean) populateBean(request);
+		StaffModel model = new StaffModel();
+
+		String op = DataUtility.getString(request.getParameter("operation"));
+		String[] ids = request.getParameterValues("ids");
+
+		try {
+
+			if (OP_SEARCH.equalsIgnoreCase(op) || "Next".equalsIgnoreCase(op) || "Previous".equalsIgnoreCase(op)) {
+
+				if (OP_SEARCH.equalsIgnoreCase(op)) {
+					pageNo = 1;
+				} else if (OP_NEXT.equalsIgnoreCase(op)) {
+					pageNo++;
+				} else if (OP_PREVIOUS.equalsIgnoreCase(op) && pageNo > 1) {
+					pageNo--;
+				}
+
+			} else if (OP_NEW.equalsIgnoreCase(op)) {
+				ServletUtility.redirect(ORSView.STAFF_CTL, request, response);
+				return;
+
+			} else if (OP_DELETE.equalsIgnoreCase(op)) {
+				pageNo = 1;
+				if (ids != null && ids.length > 0) {
+					StaffBean deletebean = new StaffBean();
+					for (String id : ids) {
+						deletebean.setId(DataUtility.getInt(id));
+						model.delete(deletebean);
+						ServletUtility.setSuccessMessage("User deleted successfully", request);
+					}
+				} else {
+					ServletUtility.setErrorMessage("Select at least one record", request);
+				}
+
+			} else if (OP_RESET.equalsIgnoreCase(op)) {
+				ServletUtility.redirect(ORSView.STAFF_LIST_CTL, request, response);
+				return;
+
+			} else if (OP_BACK.equalsIgnoreCase(op)) {
+				ServletUtility.redirect(ORSView.STAFF_LIST_CTL, request, response);
+				return;
+			}
+
+			list = model.search(bean, pageNo, pageSize);
+			next = model.search(bean, pageNo + 1, pageSize);
+
+			if (list == null || list.size() == 0) {
+				ServletUtility.setErrorMessage("No record found ", request);
+			}
+
+			ServletUtility.setList(list, request);
+			ServletUtility.setPageNo(pageNo, request);
+			ServletUtility.setPageSize(pageSize, request);
+			ServletUtility.setBean(bean, request);
+			request.setAttribute("nextListSize", next.size());
+
+			ServletUtility.forward(getView(), request, response);
+
+		} catch (ApplicationException e) {
+			e.printStackTrace();
+			ServletUtility.handleException(e, request, response);
+			return;
+		}
+	}
+
+	@Override
+	protected String getView() {
+		return ORSView.STAFF__LIST_VIEW;
+	}
+
+}
---
+++ ORSProject-04/src/main/webapp/jsp/StaffListView.jsp
@@ -0,0 +1,146 @@
+<%@page import="java.text.SimpleDateFormat"%>
+<%@page import="in.co.rays.proj4.controller.StaffListCtl"%>
+<%@page import="in.co.rays.proj4.util.HTMLUtility"%>
+<%@page import="in.co.rays.proj4.bean.StaffBean"%>
+<%@page import="java.util.Iterator"%>
+<%@page import="java.util.List"%>
+<%@page import="java.util.HashMap"%>
+<%@page import="in.co.rays.proj4.util.DataUtility"%>
+<%@page import="in.co.rays.proj4.util.ServletUtility"%>
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+	pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+	<%@include file="Header.jsp"%>
+
+	<jsp:useBean id="bean" class="in.co.rays.proj4.bean.StaffBean"
+		scope="request"></jsp:useBean>
+
+	<div align="center">
+		<h1 align="center" style="margin-bottom: -15; color: navy;">User
+			List</h1>
+
+		<div style="height: 15px; margin-bottom: 12px">
+			<h3>
+				<font color="red"><%=ServletUtility.getErrorMessage(request)%></font>
+			</h3>
+			<h3>
+				<font color="green"><%=ServletUtility.getSuccessMessage(request)%></font>
+			</h3>
+		</div>
+
+		<form action="<%=ORSView.STAFF_LIST_CTL%>" method="post">
+			<%
+				int pageNo = ServletUtility.getPageNo(request);
+				int pageSize = ServletUtility.getPageSize(request);
+				int index = ((pageNo - 1) * pageSize) + 1;
+				int nextListSize = DataUtility.getInt(request.getAttribute("nextListSize").toString());
+
+				HashMap<String, String> map = (HashMap<String, String>) request.getAttribute("map");
+				List<StaffBean> list = (List<StaffBean>) ServletUtility.getList(request);
+				Iterator<StaffBean> it = list.iterator();
+
+				if (list.size() != 0) {
+			%>
+
+			<input type="hidden" name="pageNo" value="<%=pageNo%>"> <input
+				type="hidden" name="pageSize" value="<%=pageSize%>">
+
+			<table style="width: 100%">
+				<tr>
+					<td align="center"><label><b>Full Name :</b></label> <input
+						type="text" name="fullName" placeholder="Enter Full Name"
+						value="<%=ServletUtility.getParameter("fullName", request)%>">&emsp;
+
+						<%-- 	<label><b>DOB :</b></label> <input type="date" name="dob"
+						placeholder="Enter Date"
+						value="<%=ServletUtility.getParameter("dob", request)%>">&emsp; --%>
+
+						<label><b>Previous Employer:</b></label> <input type="text"
+						name="previousEmployer" placeholder="Enter Previous Employer"
+						value="<%=ServletUtility.getParameter("previousEmployer", request)%>">&emsp;
+
+						<label><b>Division : </b></label> <%=HTMLUtility.getList("division", String.valueOf(bean.getDivision()), map)%>&emsp;
+
+						<input type="submit" name="operation"
+						value="<%=StaffListCtl.OP_SEARCH%>"> &nbsp; <input
+						type="submit" name="operation" value="<%=StaffListCtl.OP_RESET%>">
+					</td>
+				</tr>
+			</table>
+			<br>
+
+			<table border="1" style="width: 100%; border: groove;">
+				<tr style="background-color: #e1e6f1e3;">
+					<th width="5%"><input type="checkbox" id="selectall" /></th>
+					<th width="5%">S.No</th>
+					<th width="13%">Full Name</th>
+					<th width="13%">Joining Date</th>
+					<th width="23%">Division</th>
+					<th width="10%">Previous Employer</th>
+					<th width="5%">Edit</th>
+				</tr>
+
+				<%
+					while (it.hasNext()) {
+							bean = (StaffBean) it.next();
+
+							SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+							String date = sdf.format(bean.getJoiningDate());
+				%>
+
+				<tr>
+					<td style="text-align: center;"><input type="checkbox"
+						name="ids" value="<%=bean.getId()%>"></td>
+
+					<td style="text-align: center;"><%=index++%></td>
+					<td style="text-align: center; text-transform: capitalize;"><%=bean.getFullName()%></td>
+					<td style="text-align: center;"><%=date%></td>
+					<td style="text-align: center; text-transform: lowercase;"><%=bean.getDivision()%></td>
+					<td style="text-align: center;"><%=bean.getPreviousEmployer()%></td>
+					<td style="text-align: center;"><a
+						href="StaffCtl?id=<%=bean.getId()%>">Edit</a></td>
+				</tr>
+
+				<%
+					}
+				%>
+			</table>
+
+			<table style="width: 100%">
+				<tr>
+					<td style="width: 25%"><input type="submit" name="operation" value="<%=StaffListCtl.OP_PREVIOUS%>" 
+					<%=pageNo > 1 ? "" : "disabled"%>></td>
+					<td align="center" style="width: 25%"><input type="submit"
+						name="operation" value="<%=StaffListCtl.OP_NEW%>"></td>
+					<td align="center" style="width: 25%"><input type="submit"
+						name="operation" value="<%=StaffListCtl.OP_DELETE%>"></td>
+					<td style="width: 25%" align="right"><input type="submit"
+						name="operation" value="<%=StaffListCtl.OP_NEXT%>"
+						<%=nextListSize != 0 ? "" : "disabled"%>></td>
+				</tr>
+			</table>
+
+			<%
+				} else {
+			%>
+
+			<table>
+				<tr>
+					<td align="right"><input type="submit" name="operation"
+						value="<%=StaffListCtl.OP_BACK%>"></td>
+				</tr>
+			</table>
+
+			<%
+				}
+			%>
+		</form>
+	</div>
+</body>
+</html>
